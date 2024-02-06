f2d75ed222c5af4e9e7d53c42975213928a198bf
shrey10a <shreya@neolen.xyz>
2024-02-06 23:30:05 +0530

    mui installation and typography

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/material"
+
+const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant="h1">Heading 1</Typography>
+        <Typography variant="h2">Heading 2</Typography>
+        <Typography variant="h3">Heading 3</Typography>
+        <Typography variant="h4" >Heading 4</Typography>
+        <Typography variant="h5">Heading 5</Typography>
+        <Typography variant="h6">Heading 6</Typography>
+        <Typography variant="subtitle1">Subtitle 1</Typography>
+        <Typography variant="subtitle2">Subtitle 2</Typography>
+        <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aliquid cupiditate reprehenderit vero! Voluptate perferendis, eos repellendus cum sit, nihil praesentium suscipit laudantium facere eveniet consectetur similique commodi incidunt deleniti.</Typography>
+        <Typography variant="body2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste repellendus incidunt officiis laboriosam sequi ipsum in voluptates, temporibus, necessitatibus qui expedita cupiditate. Facilis nobis officiis optio blanditiis ducimus dignissimos omnis!</Typography>
+    </div>
+  )
+}
+
+export default MuiTypography
